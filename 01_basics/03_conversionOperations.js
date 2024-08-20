e3ef1565fd4bd2f715a440c35405d57835799003
aniket300495 <78916277+aniket300495@users.noreply.github.com>
2024-08-20 06:33:34 +0000

    operations updated

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -23,5 +23,49 @@
 let someNumber = true;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//  ******* Operations *******
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2*2);
+// console.log(2%2);
+
+let str1 = "ani ";
+let str2 = "patel";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2" + 1); // 121
+// console.log("1" + "2"); // 12
+// console.log(2 + 1 + "2"); //32
+
+// console.log(+true); //1
+
+let num1, num2, num3
+ 
+num1 = num2 = num3 = 2;
+// console.table([num1, num2, num3]);
+
+let gameCounter = 100;
+// console.log(gameCounter);
+gameCounter++;
+// console.log(gameCounter);
+
+gameCounter--;
+gameCounter--;
+
+// console.log(gameCounter);
+
+
+
