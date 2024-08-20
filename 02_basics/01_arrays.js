e76240a08a8db7724ed462c8b986ab2b756692a4
aniket300495 <78916277+aniket300495@users.noreply.github.com>
2024-08-20 19:00:40 +0000

    array part1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,39 @@
+// array 
+// const myArray = [0, 1, 2,3 ,4 , 5, true, 'ani'];
+const onePiece = ['luffy', 'sabo', 'zoro', 'sanji', 'chopper', 'franky', 'usopp'];
+const myArr2 = new Array(1,2,3,4,5);
+
+// console.log(myArray[2]);
+// console.log(myArr2[2]);
+// console.log(onePiece[3]);
+
+// ---------- Array Methods -------
+// myArray.push(6); //add element at end of an array
+// myArray.push(8);
+// myArray.pop(8); //removes element at end of an array
+// myArray.unshift(89); //add element at start of an array
+// myArray.shift(); //remove element at start of an array
+
+// console.log(myArray.includes(2));
+// console.log(myArray.indexOf(4));
+
+// const newArr = myArray.join();
+
+// console.log(myArray);
+// console.log(newArr);
+// console.log(typeof newArr);
+const myArray = [0, 1, 2,3 ,4 , 5, true, 'ani'];
+
+// console.log("A ", myArray);
+
+const myn1 = myArray.slice(1, 4);
+// console.log(myn1);
+
+// console.log("B", myArray);
+
+const myn2 = myArray.splice(1, 4);
+// console.log(myn2);
+// console.log("C", myArray);
+
+
+
