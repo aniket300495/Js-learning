8993f8f4b821a8417f3b19b1458a4939614ff3ac
aniket300495 <78916277+aniket300495@users.noreply.github.com>
2024-08-20 16:27:49 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,32 @@
+const name = 'ani';
+const repoCount = 4;
+
+// console.log(name + repoCount + " Value"); //outdated 
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}.`);
+
+const gameName = new String('tic-tac-toe');
+
+// console.log(gameName[0]);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('a'));
+
+// const newString = gameName.substring(0, 7);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 8);
+// console.log(anotherString);
+
+// const newStringOne = "    aniket    ";
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://google.com/google%20workspace";
+
+// console.log(url.replace('%20', '-'));
+
+// console.log(url.includes('google'));
+
+console.log(gameName.split('-'));
