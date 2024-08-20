0f51c33513cdbce588545e0303cfe3e745a29d27
aniket300495 <78916277+aniket300495@users.noreply.github.com>
2024-08-20 17:08:20 +0000

    math and num

---
+++ 01_basics/06_num-maths.js
@@ -0,0 +1,39 @@
+// const score = 35;
+// console.log(score);
+
+// const balance = new Number(145);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+// console.log(typeof balance);
+
+// const otherNumber = 123.6756457;
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//  ------ MATHS -----
+// console.log(Math);
+// console.log(Math.abs(-45));
+// console.log(Math.round(223.55));
+// console.log(Math.ceil(223.55));
+// console.log(Math.floor(223.55));
+
+// console.log(Math.sqrt(25));
+// console.log(Math.min(25, 34, 5, 67, 9));
+// console.log(Math.max(25, 34, 5, 67, 9));
+
+// console.log(Math.random());
+// console.log((Math.random()*6) + 1);
+// console.log(Math.floor(Math.random()*6) + 1);
+
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
