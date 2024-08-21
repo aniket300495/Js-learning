25444512f6f027b9015bcd85b0b7da7267a4e77c
aniket300495 <78916277+aniket300495@users.noreply.github.com>
2024-08-21 12:22:10 +0000

    functions part2

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -37,3 +37,38 @@ function loginUserMessage(username = 'sam'){
 }
 // console.log(loginUserMessage("aniket"));
 // console.log(loginUserMessage('suraj'));
+
+// below is rest operator.
+// function calculateCartPrice(...num1){
+//     return num1;
+// }
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1;
+}
+
+// console.log(calculateCartPrice(2,34, 45, 67, 56, 78));
+
+const user = {
+    username : 'ani',
+    ages:  29
+};
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} ,age is ${anyobject.age}`);
+}
+// handleObject(user);
+
+// handleObject({
+//     username: 'sam',
+//     age:30
+// });
+
+const myNewArray = [200, 300, 400, 500];
+
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+// console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue([200, 300, 100, 400]));
