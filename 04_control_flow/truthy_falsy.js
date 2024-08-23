7ae770f04931aa55d548f8e39d9bf2bbfe5c144a
aniket300495 <78916277+aniket300495@users.noreply.github.com>
2024-08-23 13:04:56 +0000

    switch , truthy and falsy

---
+++ 04_control_flow/truthy_falsy.js
@@ -0,0 +1,44 @@
+const userEmail = [];
+
+// if(userEmail){
+//     console.log("got user email");
+// }else{
+//     console.log("don't have user email");
+// }
+
+// falsy values
+// false, 0 , undefined, null, "", -0, BigInt 0n, NaN
+
+// truthy values
+// 1, [], {}, "s", "0", " ", funtion(){}
+
+// if(userEmail.length === 0){
+//     console.log("Array is empty");
+// }
+
+// const emptyObj = {};
+
+// if(Object.keys(emptyObj).length === 0){
+//     console.log("object is empty");
+// }
+
+// Nullish Coalescing operator (??): null & undefined
+// let val1;
+// val1 = 5 ?? 10;
+// val1 = null ?? 10;
+
+// console.log(val1);
+
+// val2 = undefined ?? 15;
+// console.log(` val2 : ${val2}`);
+
+// val1 = null ?? 10 ?? 20;
+// console.log(val1);
+
+//  ternary operator 
+
+//condition ? true : false
+
+const score = 34;
+
+score >= 35 ? console.log("Passed the test") : console.log("Failed the test");
