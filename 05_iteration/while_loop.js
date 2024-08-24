da41fccf4004326cb0a99df9e399509605876470
aniket300495 <78916277+aniket300495@users.noreply.github.com>
2024-08-24 08:08:21 +0000

    while loop

---
+++ 05_iteration/while_loop.js
@@ -0,0 +1,21 @@
+// let index = 0;
+// while (index <= 10) {
+//     console.log(`Value of Index is: ${index}`);
+//     // index++;
+//     index = index + 2;
+// }
+
+let myArray = ['luffy', 'zoro', 'shanks', 'boa hancock', 'nami', 'robin'];
+
+let arr = 0;
+// while(arr < myArray.length){
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr++;
+// }
+
+// do-while
+
+// do {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr++;
+// } while (arr < myArray.length);
